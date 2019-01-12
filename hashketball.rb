a7030489eca273cfdd672ad088a9054b67a93bb9
andy reyes <11710reyes@gmail.com>
2019-01-12 01:47:29 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -161,22 +161,17 @@ def team_names
 end
 
 def player_numbers(tname)
-  home_num = []
-  away_num = []
+ 
   if tname == game_hash[:home][:team_name]
-    game_hash[:home][:players].each do |player_name, player_hash|
-      home_num << player_hash[:number]
-      return home_num
-    end
+    game_hash[:home][:players].map do |player_name, player_hash|
+      player_hash[:number]
   end
-  if tname == game_hash[:away][:team_name]
-    game_hash[:away][:players].each do |player_name, player_hash|
-      away_num << player_hash[:number]
-      return away_num
+  elsif tname == game_hash[:away][:team_name]
+    game_hash[:away][:players].map do |player_name, player_hash|
+      player_hash[:number]
     end
   end
 end
-player_numbers("Brooklyn Nets")
 def player_stats(name)
   game_hash[:home][:players].each do |player_name, player_hash|
     if name == player_name
