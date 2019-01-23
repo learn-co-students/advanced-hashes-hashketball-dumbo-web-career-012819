85646067816647ec121e3862aaf89eb65345cfd6
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-23 06:32:41 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,11 @@
+require "pry"
+
 # Write your code here!
+
 def game_hash
   {
-  :home_team => {
-    :name => "Brooklyn Nets",
+  :home => {
+    :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
     :players => [
       {
@@ -62,8 +65,8 @@ def game_hash
       }
     ]
   },
-  :away_team => {
-    :name => "Charlotte Hornets",
+  :away => {
+    :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
     :players => [
       {
@@ -124,4 +127,97 @@ def game_hash
     ]
   }
 }
+end
+
+
+def num_points_scored(name)
+  points_scored = []
+
+  game_hash.each do |team, details_hash|
+    players_array = details_hash[:players]
+      players_array.each do |player_details_hash|
+        if player_details_hash[:name] == name
+          points_scored = player_details_hash[:points]
+        end
+      end
+  end
+  points_scored
+end
+
+
+def shoe_size(name)
+  players_size = []
+
+  game_hash.each do |team, details_hash|
+    players_array = details_hash[:players]
+      players_array.each do |player_details_hash|
+        if player_details_hash[:name] == name
+          players_size = player_details_hash[:shoe]
+        end
+      end
+  end
+  players_size
+end
+
+
+def team_colors (team_name)
+    colors = []
+    game_hash.each do |team, team_details_hash|
+        if team_details_hash[:team_name] == team_name
+            colors = team_details_hash[:colors].flatten
+        end
+    end
+    colors
+end
+
+
+def team_names
+  game_hash.collect do |team, team_details_hash|
+    team_details_hash[:team_name]
+  end
+end
+
+
+def player_numbers (team_name)
+  player_numbers_list = []
+  game_hash.each do |team, team_details_hash|
+    if team_details_hash[:team_name] == team_name
+      team_details_hash[:players].each do |player|
+        player.each do |key, value|
+          if key == :number
+            player_numbers_list << value
+          end
+        end
+      end
+    end
+  end
+  player_numbers_list
+end
+
+
+def player_stats(player_name)
+  player_stats = []
+  game_hash.each do |team, team_details_hash|
+    team_details_hash[:players].each do |stats|
+      if stats[:name] == player_name
+        stats.delete(:name)
+        player_stats = stats
+      end
+    end
+  end
+  player_stats
+end
+
+def big_shoe_rebounds
+  big_shoes_guy = 0
+  rebounds = 0
+    game_hash.each do |team, team_details_hash|
+      team_details_hash[:players].each do | stats |
+        if stats[:shoe] > big_shoes_guy
+          big_shoes_guy = stats[:shoe]
+          rebounds = stats[:rebounds]
+        end
+      end
+    end
+  rebounds
 end
